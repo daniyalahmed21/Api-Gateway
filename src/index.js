3fc34cb57b58e4f194279a6f0b7b403813f9f6bc
Daniyal Ahmed <da9033431@gmail.com>
2025-09-19 22:11:56 +0100

    FEAT: Added rate limiting

--- src/index.js
+++ src/index.js
@@ -2,8 +2,17 @@ import Express from "express";
 import { SERVER_CONFIG } from "./config/serverConfig.js";
 import apiRouter from "./routes/index.js";
 import Middlewares from "./middlewares/index.js";
+import { rateLimit } from "express-rate-limit";
+
+const limiter = rateLimit({
+  windowMs: 2 * 60 * 1000,
+  max: 20,
+  message: "Too many requests from this IP, please try again after a while",
+});
 
 const app = Express();
+
+app.use(limiter);
 
 app.use(Express.json());
 app.use(Express.urlencoded({ extended: true }));
