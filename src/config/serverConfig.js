fc19c1cacbffb84f89459835cc0b6faa64257432
Daniyal Ahmed <da9033431@gmail.com>
2025-09-23 16:45:27 +0100

    Added rate limiter and proxy

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -14,4 +14,6 @@ export const SERVER_CONFIG = {
   SALT_ROUNDS: process.env.SALT_ROUNDS || 10,
   JWT_SECRET_KEY: process.env.JWT_SECRET_KEY || "your_jwt_secret_key",
   JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || "1h",
+  FLIGHT_SERVICE: process.env.FlightService_URL || "http://localhost:3000/",
+  BOOKING_SERVICE: process.env.BookingService_URL || "http://localhost:4000/",
 };
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { SERVER_CONFIG } from "./config/serverConfig.js";
 import apiRouter from "./routes/index.js";
 import Middlewares from "./middlewares/index.js";
 import { rateLimit } from "express-rate-limit";
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 const limiter = rateLimit({
   windowMs: 2 * 60 * 1000,
@@ -10,9 +11,23 @@ const limiter = rateLimit({
   message: "Too many requests from this IP, please try again after a while",
 });
 
+const FlightProxyMiddleware = createProxyMiddleware({
+  target: SERVER_CONFIG.FLIGHT_SERVICE,
+  changeOrigin: true,
+  pathRewrite: { '^/flightService': '/' }
+});
+
+const bookingProxyMiddleware = createProxyMiddleware({
+  target: SERVER_CONFIG.BOOKING_SERVICE,
+  changeOrigin: true,
+  pathRewrite: { '^/bookingService': '/' }
+});
+
 const app = Express();
 
 app.use(limiter);
+app.use("/flightService", FlightProxyMiddleware);
+app.use("/bookingService", bookingProxyMiddleware);
 
 app.use(Express.json());
 app.use(Express.urlencoded({ extended: true }));
